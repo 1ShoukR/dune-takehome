9faef53545854caf5ff4bc27f6c5f02151a7adfb
1ShoukR <rahminshoukoohi@gmail.com>
2025-08-20 16:38:09 -0400

    added deployed app to CORS

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -59,7 +59,7 @@ func main() {
 	// Middleware
 	app.Use(logger.New())
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "http://localhost:3000",
+		AllowOrigins: "http://localhost:3000, https://your-backend-railway-url.railway.app/api/v1",
 		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
 		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
 	}))
