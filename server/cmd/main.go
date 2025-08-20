2ab45a50b46f559a567a32ded36a5d2a36a33e64
1ShoukR <rahminshoukoohi@gmail.com>
2025-08-20 16:56:34 -0400

    fixed cors issue

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -59,9 +59,9 @@ func main() {
 	// Middleware
 	app.Use(logger.New())
 	app.Use(cors.New(cors.Config{
-		AllowOrigins: "https://dune-takehome-production.up.railway.app, http://localhost:3000",
-		AllowHeaders: "Origin, Content-Type, Accept, Authorization",
-		AllowMethods: "GET, POST, PUT, DELETE, OPTIONS",
+		AllowOrigins:     "https://dune-takehome-production.up.railway.app, http://localhost:3000",
+		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
+		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
 		AllowCredentials: true,
 	}))
 
