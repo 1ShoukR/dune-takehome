2d0be9c8e546caf42aae743259cf8a421d3d177c
1ShoukR <rahminshoukoohi@gmail.com>
2025-08-20 17:58:06 -0400

    update

--- server/cmd/main.go
+++ server/cmd/main.go
@@ -59,7 +59,7 @@ func main() {
 	// Middleware
 	app.Use(logger.New())
 	app.Use(cors.New(cors.Config{
-		AllowOrigins:     "https://pretty-imagination-production-3bad.up.railway.app,http://localhost:3000",
+		AllowOrigins:     "https://pretty-imagination-production-3bad.up.railway.app",
 		AllowHeaders:     "Origin, Content-Type, Accept, Authorization",
 		AllowMethods:     "GET, POST, PUT, DELETE, OPTIONS",
 		AllowCredentials: true,
